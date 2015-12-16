f5b1307b2643d6d559160b0b58a348109a77aa8f
vnc456 <benazet.vincent@gmail.com>
2015-12-16 18:43:51 +0100

    terrain

--- Assets/MouvementBalle.cs
+++ Assets/MouvementBalle.cs
@@ -4,19 +4,17 @@ using UnityEngine.UI;
 
 public class MouvementBalle : MonoBehaviour {
 
-	public  GameObject text;
+	public  GameObject text ,t2;
 	Vector3 mMouseDownPos;
 	Vector3 mMouseUpPos;
-	public float speed = 50.1f;
-
-
-	/*
-	void OnMouseDown() {
-		TextMesh t= text.GetComponent<TextMesh>();
-		t.text = "collision";
-		this.GetComponent<Rigidbody> ().velocity = new Vector3(0, 10, 5);
-	}
-*/ void OnMouseDown() 
+    private Vector3 vitesse;
+    public Camera cam;
+    private Vector2 posInit,posEnd;
+    private float temp;
+    private float coefSpeed = 300;
+    private float coefDelta = 7;
+    /*
+ void OnMouseDown() 
 	{
 		mMouseDownPos = Input.mousePosition;
 		Debug.Log( "the mouse down pos is " + mMouseDownPos.y.ToString() );
@@ -35,13 +33,60 @@ public class MouvementBalle : MonoBehaviour {
 		this.GetComponent<Rigidbody> ().velocity=  -(direction * speed);
 		Debug.Log( "direction " + direction.ToString() );
 	}
-	// Use this for initialization
-	void Start () {
-	
-	}
+    */
 	
 	// Update is called once per frame
 	void Update () {
-	
+        int nbTouches = Input.touchCount;
+        if (nbTouches > 0)
+        {
+            //text.GetComponent<TextMesh>().text = " nbTouche";
+
+            for (int i = 0; i < nbTouches; i++)
+            {
+
+                Touch touch = Input.GetTouch(i);
+                TouchPhase phase = touch.phase;
+                Vector2 v2 = Vector2.zero;
+                v2 = Input.GetTouch(i).position;
+                Vector3 wp = Vector3.zero;
+                wp = cam.ScreenToWorldPoint(v2);
+
+                switch (phase)
+                {
+                    case TouchPhase.Began :
+                        posInit=touch.position;
+                        break;
+                    case TouchPhase.Ended:
+                        posEnd = touch.position;
+                        var d = posEnd - posInit;
+                        d.x /= Screen.width;
+                        d.y /= Screen.height;
+                        d.x *= coefDelta;
+                        d.y *= coefDelta;
+                        text.GetComponent<TextMesh>().text = " mon delta= " + d;
+                        vitesse.x = d.x;
+                        vitesse.y = d.y;
+                        vitesse.z = d.y;
+                        vitesse *= temp;
+                        this.GetComponent<Rigidbody>().velocity = vitesse;
+                        t2.GetComponent<TextMesh>().text = " vitesse = " + vitesse;
+                        break;
+                    case TouchPhase.Moved:
+                        if (touch.deltaPosition != Vector2.zero && touch.deltaTime!=0)
+                        {
+                            var t = touch.deltaPosition;
+                           temp= (t.magnitude/touch.deltaTime)/coefSpeed;
+                            //text.GetComponent<TextMesh>().text = " valeur speed = " + temp;
+
+                            
+
+
+                        }
+                        break;
+                    default: break;
+                }
+            }
+        }
 	}
 }
